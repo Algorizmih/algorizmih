65ceb0b5f8f96a09e3e7f88d8f03c28d46a118c7
skad12 <stephenkunle26@gmail.com>
2024-08-16 13:44:18 +0100

    initializing

---
+++ app/page.js
@@ -0,0 +1,15 @@
+import Header from "@/components/layouts/Header";
+import Banner from "@/components/themes/Banner";
+import AboutBanner from "@/components/themes/AboutBanner";
+
+export default function Home() {
+  return (
+    <>
+      <div>
+        <Header />
+        <Banner />
+        <AboutBanner />
+      </div>
+    </>
+  );
+}
---
+++ components/layouts/Header.jsx
@@ -0,0 +1,166 @@
+// components/Header.js
+"use client";
+import Image from "next/image";
+import React, { useState } from "react";
+import logo from "@/public/images/logo.png";
+
+const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+
+  const navItems = [
+    {
+      name: "About Us",
+      href: "#",
+      subMenu: [
+        { name: "Our Team", href: "#" },
+        { name: "Our History", href: "#" },
+        { name: "Careers", href: "#" },
+      ],
+    },
+    { name: "Services", href: "#" },
+    { name: "Medical Data Flow", href: "/medical" },
+    { name: "AI Labs", href: "/labs" },
+    { name: "R&D", href: "/press" },
+    { name: "Press", href: "/press" },
+    { name: "Contact", href: "#" },
+  ];
+
+  return (
+    <nav className="bg-white py-5">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex justify-between h-16">
+          <div className="flex-shrink-0 flex items-center">
+            <a href="/" className="text-black text-xl font-bold">
+              <Image alt="" width={150} height={100} src={logo} />
+            </a>
+          </div>
+          <div className="hidden md:flex items-center space-x-4 border border-gray-600 rounded-full px-5">
+            {navItems.map((item) =>
+              item.subMenu ? (
+                <div
+                  key={item.name}
+                  className="relative "
+                  onMouseEnter={() => setDropdownOpen(true)}
+                  onMouseLeave={() => setDropdownOpen(false)}
+                >
+                  <button className="text-black  px-3 py-2  rounded-md text-sm font-medium">
+                    {item.name}
+                  </button>
+                  {dropdownOpen && (
+                    <div className="absolute left-0 mt-2 w-48 bg-white shadow-lg rounded-md py-2 z-10">
+                      {item.subMenu.map((subItem) => (
+                        <a
+                          key={subItem.name}
+                          href={subItem.href}
+                          className="block px-4  text-gray-800 hover:bg-gray-200"
+                        >
+                          {subItem.name}
+                        </a>
+                      ))}
+                    </div>
+                  )}
+                </div>
+              ) : (
+                <a
+                  key={item.name}
+                  href={item.href}
+                  className="text-black px-3 py-2  rounded-md text-sm font-medium"
+                >
+                  {item.name}
+                </a>
+              )
+            )}
+          </div>
+          <div className="-mr-2 flex md:hidden">
+            <button
+              onClick={() => setIsOpen(!isOpen)}
+              type="button"
+              className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+              aria-controls="mobile-menu"
+              aria-expanded="false"
+            >
+              <span className="sr-only">Open main menu</span>
+              {!isOpen ? (
+                <svg
+                  className="block h-6 w-6"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h16M4 18h16"
+                  />
+                </svg>
+              ) : (
+                <svg
+                  className="block h-6 w-6"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              )}
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <div
+        className={`${isOpen ? "block" : "hidden"} md:hidden`}
+        id="mobile-menu"
+      >
+        <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+          {navItems.map((item) =>
+            item.subMenu ? (
+              <div key={item.name} className="relative">
+                <button
+                  onClick={() => setDropdownOpen(!dropdownOpen)}
+                  className="text-black hover:bg-gray-300 block px-3 py-2 rounded-md text-base font-medium w-full text-left"
+                >
+                  {item.name}
+                </button>
+                {dropdownOpen && (
+                  <div className="bg-gray-800">
+                    {item.subMenu.map((subItem) => (
+                      <a
+                        key={subItem.name}
+                        href={subItem.href}
+                        className="block px-3 py-2 text-black  hover:bg-gray-300 rounded-md"
+                      >
+                        {subItem.name}
+                      </a>
+                    ))}
+                  </div>
+                )}
+              </div>
+            ) : (
+              <a
+                key={item.name}
+                href={item.href}
+                className="text-black hover:bg-gray-700 block px-3 py-2 rounded-md text-base font-medium"
+              >
+                {item.name}
+              </a>
+            )
+          )}
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
